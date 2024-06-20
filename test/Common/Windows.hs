ce64657936035c1db16d3126d017f7b34912bca7
Mann mit Hut <github+mmhat@with-h.at>
2024-06-20 23:56:55 +0200

    Testsuite test: Moved TH stuff to own modules

--- test/Common/Windows.hs
+++ test/Common/Windows.hs
@@ -1,25 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE QuasiQuotes #-}
 
-#define PLATFORM_NAME   Windows
-#define IS_WINDOWS      True
+#define PLATFORM_NAME Windows
 #include "Include.hs"
-
-qqAbsDir :: FilePath
-qqAbsDir = checkInstantiated [absdir|C:\foo\|]
-
-qqAbsFile :: FilePath
-qqAbsFile = checkInstantiated [absdir|C:\foo|]
-
-thAbsDir :: FilePath
-thAbsDir = checkInstantiated $(mkAbsDir "C:\\foo\\")
-
-thAbsFile :: FilePath
-thAbsFile = checkInstantiated $(mkAbsFile "C:\\foo")
-
-liftAbsDir :: FilePath
-liftAbsDir = checkInstantiated $(TH.lift (Path "C:\\foo\\" :: Path Abs Dir))
-
-liftAbsFile :: FilePath
-liftAbsFile = checkInstantiated $(TH.lift (Path "C:\\foo" :: Path Abs File))
-
