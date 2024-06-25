63a1f5d8305a0f97c5f03474d90909d7b2cced5b
Mann mit Hut <github+mmhat@with-h.at>
2024-06-25 20:24:33 +0200

    Better GenValid instance for PosixPath/WindowsPath

--- validity-test-ospath/OsPath/Gen/Posix.hs
+++ validity-test-ospath/OsPath/Gen/Posix.hs
@@ -2,4 +2,7 @@
 
 #define PLATFORM_NAME        Posix
 #define PLATFORM_PATH        PosixPath
+#define PLATFORM_PATH_SINGLE 'PosixPath'
+#define PLATFORM_CHAR        PosixChar
+#define PLATFORM_WORD        Word8
 #include "Include.hs"
--- validity-test-ospath/OsPath/Gen/Windows.hs
+++ validity-test-ospath/OsPath/Gen/Windows.hs
@@ -2,4 +2,7 @@
 
 #define PLATFORM_NAME        Windows
 #define PLATFORM_PATH        WindowsPath
+#define PLATFORM_PATH_SINGLE 'WindowsPath'
+#define PLATFORM_CHAR        WindowsChar
+#define PLATFORM_WORD        Word16
 #include "Include.hs"
